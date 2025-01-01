ef32b84104e61bc5334016d090c9ab04d5bb67f0
Rohan <walkerrandy930@gmail.com>
2025-01-01 23:32:35 +0545

    Adding few new lines

--- index.php
+++ index.php
@@ -5,5 +5,8 @@ require_once 'src/Hello.php';
 
 $hello = new Silarhi\Hello();
 
-echo $hello->display()
+echo $hello->display();
+echo "The project has been completed";
+echo "Learned some new things"
+
 ?>
